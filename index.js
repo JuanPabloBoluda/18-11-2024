45d874bc783e751aa09af22d0a2773400c7788b4
JuanPabloBoluda <164278699+JuanPabloBoluda@users.noreply.github.com>
2024-08-12 23:09:42 +0000

    12/08/24-2

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const anoNascimento = 1995;
-const idade = 29;
+const anoNascimento = 2005;
+let idade = 19;
 let anoAtual = 2024;
-
+const nomeCompleto = "Juan Pablo"; 
 anoAtual =  anoAtual + 1;
+
+console.log("Meu nome é", nomeCompleto + " Nascido em " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 
 console.log(anoAtual);
 
-
+idade = idade + 1  
+console.log (idade);
 
 
