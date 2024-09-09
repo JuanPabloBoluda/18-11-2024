3fc1eff9383827e14b4335f6f5636b84342b82ab
JuanPabloBoluda <164278699+JuanPabloBoluda@users.noreply.github.com>
2024-09-09 23:02:46 +0000

    09/09/24

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const anoNascimento = 2005;
-let idade = 19;
-let anoAtual = 2024;
+let idade = 18;
+let anoAtual = 2023;
 const nomeCompleto = "Juan Pablo"; 
 anoAtual =  anoAtual + 1;
 
@@ -13,3 +13,23 @@ idade = idade + 1
 console.log (idade);
 
 
+//Agencia de viagens paris, Canadá, Dubai, Maldivas, tokyo 
+const listaDeDestinos = new Array( 
+  "Paris", 
+  "Canadá", 
+  "Dubai", 
+  "Maldivas", 
+  "Tokyo" 
+);
+
+const idadeComprador = 17;
+
+if(idadeComprador >=18){
+  console.log("comprador maior de idade");
+  console.log("destinos disponíveis: " + listaDeDestinos);
+} else{
+
+(idadeComprador < 18){
+  console.log("Não é permitido comprar a passagem para menores de idade");
+  console.log ("você é menor de idade");
+}
