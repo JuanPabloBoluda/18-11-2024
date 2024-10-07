bd692cb0c7380969344f9f8a61fcb84ba1f6355b
JuanPabloBoluda <164278699+JuanPabloBoluda@users.noreply.github.com>
2024-10-07 23:03:53 +0000

    07/10/24

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const prompt = require('prompt-sync');
 const entrada = prompt();
 
 const anoNascimento = 2005;
-let idade = 18;
-let anoAtual = 2023;
+let idade = 19;
+let anoAtual = 2024;
 const nomeCompleto = "Juan Pablo"; 
-anoAtual =  anoAtual + 1;
+
 
 console.log("Meu nome é", nomeCompleto + " Nascido em " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
+
+anoAtual =  anoAtual + 1;
 
 
 console.log(anoAtual);
@@ -15,6 +17,16 @@ console.log(anoAtual);
 idade = idade + 1  
 console.log (idade);
 
+
+console.log ("-----------------------------------------");
+console.log ("-------------Âgencia de viagens----------");
+console.log ("-----------------------------------------");
+console.log ("                                         ");
+
+console.log ("-----------------------------------------");
+console.log ("--------------Area de login--------------");
+console.log ("-----------------------------------------");
+console.log ("                                         ");
 
 //Agencia de viagens paris, Canadá, Dubai, Maldivas, tokyo 
 const listaDeDestinos = new Array( 
@@ -36,17 +48,30 @@ while (login != loginCerto || senha != senhaCerta){
   login = entrada("Qual é o seu username?");
   senha = entrada("Qual é a sua senha?");
 }
+console.log ("");
+console.log ("-----------------------------------------");
+console.log ("-------------Identificação---------------");
+console.log ("-----------------------------------------");
+console.log ("                                         ");
 
 var nomeComprador = entrada("Digite seu nome: ");
 var idadeComprador = entrada("Qual a sua idade?");
 
 
+console.log ("");
+console.log ("-----------------------------------------");
+console.log ("-------------Lista de vôos---------------");
+console.log ("--------------Disponiveis----------------");
+console.log ("-----------------------------------------");
+console.log ("");
+
 if(idadeComprador >=18){
   console.log ("Olá senhor(a) " + nomeComprador);
   console.log("comprador maior de idade");
   console.log("Escolha seu destino: ");
-  var contador =0;
+  var contador = 0;
 
+  
   while(contador <5){
 
     console.log(listaDeDestinos[contador]);
